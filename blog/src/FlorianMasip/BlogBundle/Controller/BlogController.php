08b3e6afc6815f7b86fc79a6bb751bad4c1b4df5
F-lorian <florian.masip@gmail.com>
2015-10-27 18:08:16 +0100

    ajout des pages manquantes, d'un header et d'un footer

--- blog/src/FlorianMasip/BlogBundle/Controller/BlogController.php
+++ blog/src/FlorianMasip/BlogBundle/Controller/BlogController.php
@@ -8,11 +8,22 @@ class BlogController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('BlogBundle:Blog:index.html.twig');
+        return $this->render('BlogBundle:Default:index.html.twig');
+    }
+
+    public function blogAction($id)
+    {
+        return $this->render('BlogBundle:Blog:blog.html.twig', array('id' => $id));
+    }
+
+    public function registerAction()
+    {
+        return $this->render('BlogBundle:Blog:register.html.twig');
     }
 
     public function postAction($id)
     {
         return $this->render('BlogBundle:Blog:post.html.twig', array('id' => $id));
     }
+
 }

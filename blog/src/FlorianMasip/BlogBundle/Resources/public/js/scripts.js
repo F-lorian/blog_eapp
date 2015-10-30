ba9eeb29d28c2078ffbc796052689e5bf5e98497
F-lorian <florian.masip@gmail.com>
2015-10-30 18:56:46 +0100

    ajout d'un block pour l'affichage des résultats de la recherche

---
+++ blog/src/FlorianMasip/BlogBundle/Resources/public/js/scripts.js
@@ -0,0 +1,14 @@
+function toggleClass(obj,_class){
+
+	$(obj).toggleClass(_class);
+}
+
+function addClass(obj,_class){
+
+	$(obj).addClass(_class);
+}
+
+function removeClass(obj,_class){
+
+	$(obj).removeClass(_class);
+}

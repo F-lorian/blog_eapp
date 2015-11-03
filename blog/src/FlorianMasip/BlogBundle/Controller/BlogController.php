fc26bdb5b61d350e65c173ded08159a87e691126
F-lorian <florian.masip@gmail.com>
2015-11-03 16:22:36 +0100

    ajout page pour modifier un blog

--- blog/src/FlorianMasip/BlogBundle/Controller/BlogController.php
+++ blog/src/FlorianMasip/BlogBundle/Controller/BlogController.php
@@ -21,6 +21,11 @@ class BlogController extends Controller
         return $this->render('BlogBundle:Blog:register.html.twig');
     }
 
+    public function editAction($id_blog)
+    {
+        return $this->render('BlogBundle:Blog:register.html.twig', array('id_blog' => $id_blog));
+    }
+
     public function postAction($id_blog, $id_post)
     {
         return $this->render('BlogBundle:Blog:post.html.twig', array('id_blog' => $id_blog, 'id_post' => $id_post));
